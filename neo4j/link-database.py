99a9640c7961275a6ba7f9968226327317631a96
Zhang Beihai <1402102283@qq.com>
2024-02-10 08:19:17 +0000

    添加类型

--- neo4j/link-database.py
+++ neo4j/link-database.py
@@ -1,3 +1,21 @@
-from py2neo import Graph
+from py2neo import Graph, Node, Relationship
 
 graph = Graph("neo4j+s://e9146ca1.databases.neo4j.io", auth=("neo4j", "fkbMXPiZCKv_6aLFk5SeDb19qZbi34Q1iekL046mx78"))
+
+
+# 节点类型
+
+from abc import ABC, abstractmethod
+
+class AbsGNode(ABC):
+
+    @abstractmethod
+    def get_data() -> dict:
+        pass
+
+
+LABEL = "标签"
+METHOD = "方法"
+PACKAGE = "包"
+CLASS = "类"
+FUNCTION = "函数"
